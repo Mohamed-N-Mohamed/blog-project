a09afe28413454bab88cff119c44907de29d6089
Mohamed Mohamed <63976154+Mohamed-N-Mohamed@users.noreply.github.com>
2022-08-05 16:19:32 +0100

    added firebase

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import './App.css';
 
+
 function App() {
   return (
     <div className="App">
+      <h1>Hello word</h1>
 
     </div>
   );
---
+++ src/firebase/config.js
@@ -0,0 +1,19 @@
+import { initializeApp } from "firebase/app"
+import { getAuth, GoogleAuthProvider  } from "firebase/auth"
+import { getFirestore } from 'firebase/firestore'
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDgeJFObTWxWfDiWgKrmO6wFjYig2wtNao",
+  authDomain: "book-77d03.firebaseapp.com",
+  projectId: "book-77d03",
+  storageBucket: "book-77d03.appspot.com",
+  messagingSenderId: "29862994969",
+  appId: "1:29862994969:web:7f2e9b61eab12cdcf27688"
+};
+
+const app = initializeApp(firebaseConfig)
+const auth = getAuth(app)
+const provider = new GoogleAuthProvider()
+const db = getFirestore()
+
+export { auth, provider, storage}
